042ae044902f65f149f484cc64800932bd73e331
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-08-25 17:00:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+
+Function to calculate and update the total price
+function updateTotalPrice() {
+  const prices = document.querySelectorAll('.price');
+  let total = 0;
+
+  prices.forEach(priceElement => {
+    total += parseFloat(priceElement.textContent);
+  });
+
+  const totalRow = document.createElement('tr');
+  totalRow.innerHTML = `<td><strong>Total:</strong></td><td>${total.toFixed(2)}</td>`;
+
+  const table = document.getElementById('groceryTable');
+  table.appendChild(totalRow);
+}
+
+// Call the function initially to calculate and display the total price
+updateTotalPrice();
+
+	
   
 };
 
