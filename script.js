db319b08a88e0a8beada98bd25ff295edd095f2d
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-08-27 08:09:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  function calculateTotalPrice() {
-      const priceElements = document.querySelectorAll('.prices');
+      const priceElements = document.querySelectorAll('.price');
       let total = 0;
 
       priceElements.forEach(priceElement => {
